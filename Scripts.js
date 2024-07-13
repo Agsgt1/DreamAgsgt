043962be661f8b530b81614d8269310b12abef4d
Agsgt1 <138738952+Agsgt1@users.noreply.github.com>
2024-07-13 12:00:19 -0600

    Create Scripts.js

---
+++ Scripts.js
@@ -0,0 +1,16 @@
+function We(a,b) {
+  if ((a = false) and (b = false)) {
+    return false
+  } else if ((a = false) and (b = true)) {
+    return false
+  } else if ((a = true) and (b = false)) {
+    return true
+  } else {
+    return true
+  }
+}
+
+function runner() {
+  //put your script here
+}
+
